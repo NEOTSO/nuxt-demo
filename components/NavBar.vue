4de132e1972a71240d6c5d8cffc1c4c1752e9bb1
NEOTSO <neo.tso@outlook.com>
2020-11-27 01:12:36 +0800

    dynamic head info

---
+++ components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="nav">
+        <nuxt-link to="/">Home</nuxt-link>
+        <nav>
+            <nuxt-link to="/create">create</nuxt-link>
+            <nuxt-link to="/test">test</nuxt-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+};
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>hi, test</h1>
+</template>
