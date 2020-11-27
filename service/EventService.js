d38afa539b21e6b2e4a3baa5680b75701ac08a31
NEOTSO <neotso.c@gmail.com>
2020-11-27 16:51:44 +0800

    fix https bug

--- service/EventService.js
+++ service/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://my-json-server.typicode.com/NEOTSO/nuxt-demo",
+    baseURL: "https://my-json-server.typicode.com/NEOTSO/nuxt-demo",
     withCredentials: false,
     headers: {
         Accept: "application/json",
